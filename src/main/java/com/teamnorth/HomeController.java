1cd9df5b78d6637f4cfdf63d9bd322e285eb74b8
dsolowitz <dangydave68@gmail.com>
2019-11-22 19:17:14 -0500

    Add files via upload
    Added controller for ebay item recc page, and ebay items page.

---
+++ src/main/java/com/teamnorth/HomeController.java
@@ -0,0 +1,74 @@
+package com.teamnorth.controllers;
+
+import com.teamnorth.data.Event;
+import com.teamnorth.data.db.hibernate.HibernateEventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+
+@Controller
+public class HomeController {
+
+
+
+
+
+        @RequestMapping(value="/", method= RequestMethod.GET)
+        public ModelAndView index() {
+            return new ModelAndView("homepage");
+        }
+
+        @RequestMapping(value = "/templates/nav.html", method= RequestMethod.GET)
+        public ModelAndView navPage(){
+            return new ModelAndView("nav");
+        }
+
+        @RequestMapping(value = "/templates/calendar.html", method = RequestMethod.GET)
+        public ModelAndView calPage(){
+            return new ModelAndView("calendar");
+        }
+
+
+
+        @RestController
+        class EventController {
+
+            @Autowired
+            HibernateEventService eventRepository;
+
+            @PostMapping(value = "/event")
+            public @ResponseBody  void addEvent(@RequestBody Event event) {
+                System.out.println(event.toString());
+
+                eventRepository.save(event);
+
+            }
+
+            @RequestMapping(value = "/event", method = RequestMethod.DELETE)
+            public void removeEvent(@RequestBody Event event) {
+                eventRepository.delete(event);
+            }
+
+
+
+            @RequestMapping(value="/allevents", method=RequestMethod.GET, produces = {"application/json"})
+            public List<Event> allEvents() {
+                return eventRepository.findAll();
+            }
+
+
+            @RequestMapping(value = "/ebay", method = RequestMethod.GET)
+            public ModelAndView ebayPage(){
+                return new ModelAndView( "ebay");
+            }
+        }
+}
+
+
+
+
+
