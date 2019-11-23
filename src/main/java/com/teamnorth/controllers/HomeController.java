99e7f6289baf8b01725128e43d0d101bebbb6ca4
david <ds438@duke.edu>
2019-11-22 19:39:58 -0500

    Fixed controllers

--- src/main/java/com/teamnorth/controllers/HomeController.java
+++ src/main/java/com/teamnorth/controllers/HomeController.java
@@ -1,114 +1,85 @@
 package com.teamnorth.controllers;
 
 import com.teamnorth.data.Event;
-import com.teamnorth.data.Questionnaire;
-import com.teamnorth.data.db.EventRepository;
 import com.teamnorth.data.db.hibernate.HibernateEventService;
-import com.teamnorth.data.db.hibernate.HibernateQuestionnaireService;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
-import javax.validation.Valid;
 
 @Controller
 public class HomeController {
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public ModelAndView index() {
-		return new ModelAndView("homepage");
-	}
 
-	@RequestMapping(value = "/questionnaire", method = RequestMethod.GET)
-	public ModelAndView questionnairePage() {
-		return new ModelAndView("questionnaire");
-	}
 
-	@RequestMapping(value = "/calendar", method = RequestMethod.GET)
-	public ModelAndView calPage() {
-		return new ModelAndView("calendar");
-	}
 
-	@RequestMapping(value = "/about", method = RequestMethod.GET)
-	public ModelAndView aboutPage() {
-		return new ModelAndView("about");
-	}
 
-	@RequestMapping(value = "/contact", method = RequestMethod.GET)
-	public ModelAndView contactPage() {
-		return new ModelAndView("contact");
-	}
+        @RequestMapping(value="/", method= RequestMethod.GET)
+        public ModelAndView index() {
+            return new ModelAndView("homepage");
+        }
 
-	
-	@RestController
-	class EventController {
-		/*
-		 * Handles Event posting and repository changes
-		 */
+        @RequestMapping(value = "/templates/questionnaire.html", method= RequestMethod.GET)
+        public ModelAndView navPage(){
+            return new ModelAndView("questionnaire");
+        }
 
-		@Autowired
-		HibernateEventService eventRepository;
+        @RequestMapping(value = "/templates/calendar.html", method = RequestMethod.GET)
+        public ModelAndView calPage(){
+            return new ModelAndView("calendar");
+        }
 
-		@PostMapping(value = "/event")
-		public @ResponseBody void addEvent(@RequestBody Event event) {
-			System.out.println(event.toString());
+    @RequestMapping(value = "/templates/about.html", method = RequestMethod.GET)
+    public ModelAndView aboutPage(){
+        return new ModelAndView("about");
+    }
 
-			eventRepository.save(event);
+    @RequestMapping(value = "/templates/contact.html", method = RequestMethod.GET)
+    public ModelAndView contactPage(){
+        return new ModelAndView("contact");
+    }
 
-		}
 
-		@RequestMapping(value = "/event", method = RequestMethod.DELETE)
-		public void removeEvent(@RequestBody Event event) {
-			eventRepository.delete(event);
-		}
 
-		@RequestMapping(value = "/allevents", method = RequestMethod.GET, produces = { "application/json" })
-		public List<Event> allEvents() {
-			return eventRepository.findAll();
-		}
-	}
 
-	
-	@RestController
-	class QuestionnaireController {
-		/*
-		 * Handles Questionnaire posting and repository changes
-		 */
+    @RestController
+        class EventController {
 
-		@Autowired
-		HibernateQuestionnaireService qRepo;
+            @Autowired
+            HibernateEventService eventRepository;
 
-		@PostMapping(value = "/questionnaire")
-		public @ResponseBody ModelAndView addQuestionnaire(@RequestBody @Valid Questionnaire q, Errors errors) {
-			// Catches errors
-			if (errors.hasErrors()) {
-				// Return user to the questionnaire if errors occur.
-				return new ModelAndView("questionnaire");
-			}
-			// Log additions
-			System.out.println(q.toString());
-			// Save questionnaire POST to qRepo
-			qRepo.save(q);
-			// Return user to calendar if everything goes as planned.
-			return new ModelAndView("calendar");
-		}
+            @PostMapping(value = "/event")
+            public @ResponseBody  void addEvent(@RequestBody Event event) {
+                System.out.println(event.toString());
 
-		@RequestMapping(value = "/questionnaire", method = RequestMethod.DELETE)
-		// Delete request will attempt to delete supplied Questionnaire
-		public void removeQuestionnaire(@RequestBody Questionnaire q) {
-			qRepo.delete(q);
-		}
+                eventRepository.save(event);
 
-		@RequestMapping(value = "/allquestionnaires", method = RequestMethod.GET, produces = { "application/json" })
-		// /allquestionnaires will attempt to provide a list of all questionnaires found
-		public List<Questionnaire> allQuestionnaires() {
-			return qRepo.findAll();
-		}
-	}
+            }
+
+            @RequestMapping(value = "/event", method = RequestMethod.DELETE)
+            public void removeEvent(@RequestBody Event event) {
+                eventRepository.delete(event);
+            }
+
+
+
+            @RequestMapping(value="/allevents", method=RequestMethod.GET, produces = {"application/json"})
+            public List<Event> allEvents() {
+                return eventRepository.findAll();
+            }
+
+
+            @RequestMapping(value = "/templates/ebay.html", method = RequestMethod.GET)
+            public ModelAndView ebayPage(){
+                return new ModelAndView( "ebay");
+            }
+        }
 }
+
+
+
+
+
